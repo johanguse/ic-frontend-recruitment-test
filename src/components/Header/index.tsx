27c757d30818dabbb4d7f549bb025610d86c9afd
Johan Guse <johanguse@gmail.com>
2021-06-01 17:50:13 -0300

    start header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <header className="header"></header>
+    </>
+  );
+};
+export default Header;
