7fa9e833f265ffb8133f9432638fa89977b25ead
Johan Guse <johanguse@gmail.com>
2021-06-01 18:42:35 -0300

    start header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
+import logo from 'assets/images/logo_instacarro.png';
 
 const Header = () => {
   const [open, setOpen] = useState(false);
 
   return (
     <>
-      <header className="header">teste</header>
+      <header className="container header">
+        <div className="logo">
+          <img src={logo} title="Logo InstaCarro" alt="InstaCarro" />
+        </div>
+        <div className="phone">
+          <a href="tel:+551135693465" className="tel">
+            (11) 3569-3465
+          </a>
+        </div>
+        <div className="user-login">
+          <p>User Test</p>
+        </div>
+      </header>
     </>
   );
 };
