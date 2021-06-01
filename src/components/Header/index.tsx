aad7aaf872aec1e25ddcacfec04acf7f8f24919b
Johan Guse <johanguse@gmail.com>
2021-06-01 18:04:56 -0300

    rename app index and add colors variables

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ const Header = () => {
 
   return (
     <>
-      <header className="header"></header>
+      <header className="header">teste</header>
     </>
   );
 };
---
+++ src/containers/App/index.tsx
@@ -0,0 +1,27 @@
+import Header from 'components/Header';
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
