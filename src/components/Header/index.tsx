20683bb1972d3a13bbe2d06f9d8e92510cc2d81f
Johan Guse <johanguse@gmail.com>
2021-06-01 19:14:20 -0300

    working on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import logo from 'assets/images/logo_instacarro.png';
+import { ReactComponent as IconPhone } from 'assets/images/phone.svg';
+import { ReactComponent as IconUser } from 'assets/images/user.svg';
+import { ReactComponent as IconCaret } from 'assets/images/caret.svg';
 
 const Header = () => {
   const [open, setOpen] = useState(false);
@@ -11,12 +14,15 @@ const Header = () => {
           <img src={logo} title="Logo InstaCarro" alt="InstaCarro" />
         </div>
         <div className="phone">
-          <a href="tel:+551135693465" className="tel">
-            (11) 3569-3465
+          <a href="tel:+551135693465" className="phone__link">
+            <IconPhone className="phone__link-icon" />
+            <span className="phone__link-text">(11) 3569-3465</span>
           </a>
         </div>
         <div className="user-login">
+          <IconUser />
           <p>User Test</p>
+          <IconCaret className="user-login__caret" />
         </div>
       </header>
     </>
