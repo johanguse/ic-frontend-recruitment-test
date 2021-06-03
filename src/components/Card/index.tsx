d65b03f5f426c29c85171167019efe8d3e89cbd0
Johan Guse <johanguse@gmail.com>
2021-06-03 12:49:04 -0300

    remove dead code

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const Card = () => {
+  return <div className="card">teste</div>;
+};
+
+export default Card;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,31 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import logo from 'assets/images/logo_instacarro.png';
 import { ReactComponent as IconPhone } from 'assets/images/phone.svg';
 import { ReactComponent as IconUser } from 'assets/images/user.svg';
 import { ReactComponent as IconCaret } from 'assets/images/caret.svg';
 
 const Header = () => {
-  const [open, setOpen] = useState(false);
-
   return (
-    <>
-      <header className="container header">
-        <div className="logo">
-          <img src={logo} title="Logo InstaCarro" alt="InstaCarro" />
-        </div>
-        <div className="phone">
-          <a href="tel:+551135693465" className="phone__link">
-            <IconPhone className="phone__link-icon" />
-            <span className="phone__link-text">(11) 3569-3465</span>
-          </a>
-        </div>
-        <div className="user-login">
-          <IconUser />
-          <p>User Test</p>
-          <IconCaret className="user-login__caret" />
-        </div>
-      </header>
-    </>
+    <header className="container header">
+      <div className="logo">
+        <img src={logo} title="Logo InstaCarro" alt="InstaCarro" />
+      </div>
+      <div className="phone">
+        <a href="tel:+551135693465" className="phone__link">
+          <IconPhone className="phone__link-icon" />
+          <span className="phone__link-text">(11) 3569-3465</span>
+        </a>
+      </div>
+      <div className="user-login">
+        <IconUser />
+        <p>User Test</p>
+        <IconCaret className="user-login__caret" />
+      </div>
+    </header>
   );
 };
 export default Header;
