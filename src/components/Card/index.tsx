0a2ecb9ab9570b48eb5b85fe8f9049c389ae4e04
Johan Guse <johanguse@gmail.com>
2021-06-03 15:15:16 -0300

    add rect countdown

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,16 +1,59 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import Countdown from 'react-countdown';
 import { CarType } from 'models/car.interface';
 
 interface CardProps {
   car: CarType;
 }
 
+function msToReadableTime(time: number) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+
+  const hours = Math.floor((time / hour) % 24);
+  const minutes = Math.floor((time / minute) % 60);
+  const seconds = Math.floor((time / second) % 60);
+
+  return `${hours}:${minutes}:${seconds}`;
+}
+
 const Card: FC<CardProps> = ({ car }) => {
+  const allBids = car.bids;
+
+  console.log(allBids);
+  console.log(msToReadableTime(car.remainingTime));
+
+  const Completionist = () => <span>Leilão Encerrado</span>;
+
+  const renderer = ({
+    hours,
+    minutes,
+    seconds,
+    completed,
+  }: {
+    [key: string]: any;
+  }) => {
+    if (completed) {
+      return <Completionist />;
+    }
+    return (
+      <span>
+        {hours}:{minutes}:{seconds}
+      </span>
+    );
+  };
+
   return (
     <>
-      <div className="card">
+      <div className="col-1-of-3 card">
         <p>{car.model}</p>
         <p>{car.make}</p>
+        <Countdown
+          date={Date.now() + car.remainingTime}
+          zeroPadTime={2}
+          renderer={renderer}
+        />
       </div>
     </>
   );
