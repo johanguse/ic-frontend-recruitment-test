446ae11f2130d49de1e48429a7d8e0fe1f71d6b2
Johan Guse <johanguse@gmail.com>
2021-06-06 16:26:34 -0300

    finish header desktop

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,17 +8,22 @@ const Header = () => {
   return (
     <header className="container header">
       <div className="logo">
-        <img src={logo} title="Logo InstaCarro" alt="InstaCarro" />
+        <img
+          src={logo}
+          title="Logo InstaCarro"
+          alt="InstaCarro"
+          className="logo__img"
+        />
       </div>
       <div className="phone">
         <a href="tel:+551135693465" className="phone__link">
           <IconPhone className="phone__link-icon" />
-          <span className="phone__link-text">(11) 3569-3465</span>
+          <span className="phone__link-text">(11) 3569 - 3465</span>
         </a>
       </div>
       <div className="user-login">
-        <IconUser />
-        <p>User Test</p>
+        <IconUser className="user-login__user-icon" />
+        <p className="user-login__user-name">User Test</p>
         <IconCaret className="user-login__caret" />
       </div>
     </header>
